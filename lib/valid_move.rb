b940b4aaca766f00da1ba61987acca5c425575d6
Santiago Betancur <santi-c-b@outlook.com>
2018-11-25 17:09:40 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if index >= 0 && index <= 8 then
-    return true
+    return position_taken?(board, index)
   else
     return false
   end
